9bb2cd4855b0a9fb95bf53bff2d29f57bd93dd0b
ABIDULLAH786 <abidullah.bsef18@iba-suk.edu.pk>
2020-07-21 20:15:49 +0500

    initial commit

---
+++ hms.sql
@@ -0,0 +1,259 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hms
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `boys_hostel`
+--
+
+DROP TABLE IF EXISTS `boys_hostel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `boys_hostel` (
+  `hostel_id` int NOT NULL,
+  `no_of_rooms` int NOT NULL,
+  `no_of_student` int NOT NULL,
+  `annual_expences` int NOT NULL,
+  `location` varchar(35) NOT NULL,
+  KEY `hostel_id` (`hostel_id`),
+  CONSTRAINT `boys_hostel_ibfk_1` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boys_hostel`
+--
+
+LOCK TABLES `boys_hostel` WRITE;
+/*!40000 ALTER TABLE `boys_hostel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `boys_hostel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fee`
+--
+
+DROP TABLE IF EXISTS `fee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fee` (
+  `fee_month_year` varchar(40) NOT NULL,
+  `fee_status` varchar(40) NOT NULL,
+  `student_id` int NOT NULL,
+  `student_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`fee_month_year`,`student_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `fee_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fee`
+--
+
+LOCK TABLES `fee` WRITE;
+/*!40000 ALTER TABLE `fee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `girls_hostel`
+--
+
+DROP TABLE IF EXISTS `girls_hostel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `girls_hostel` (
+  `hostel_id` int NOT NULL,
+  `no_of_rooms` int NOT NULL,
+  `no_of_student` int NOT NULL,
+  `annual_expences` int NOT NULL,
+  `location` varchar(35) NOT NULL,
+  KEY `hostel_id` (`hostel_id`),
+  CONSTRAINT `girls_hostel_ibfk_1` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `girls_hostel`
+--
+
+LOCK TABLES `girls_hostel` WRITE;
+/*!40000 ALTER TABLE `girls_hostel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `girls_hostel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hostel`
+--
+
+DROP TABLE IF EXISTS `hostel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hostel` (
+  `hostel_id` int NOT NULL,
+  `no_of_rooms` int NOT NULL,
+  `no_of_student` int NOT NULL,
+  `annual_expences` int NOT NULL,
+  `location` varchar(35) NOT NULL,
+  `hostetl_status` varchar(10) NOT NULL,
+  PRIMARY KEY (`hostel_id`),
+  CONSTRAINT `hostel_chk_1` CHECK ((`hostel_id` > 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hostel`
+--
+
+LOCK TABLES `hostel` WRITE;
+/*!40000 ALTER TABLE `hostel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hostel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mess`
+--
+
+DROP TABLE IF EXISTS `mess`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mess` (
+  `mess_incharge_Id` varchar(20) NOT NULL,
+  `monthly_avg_expence` varchar(30) NOT NULL,
+  `mess_bf_timing` varchar(45) NOT NULL,
+  `mess_dinner_timing` varchar(45) NOT NULL,
+  `sunday_bf_timing` varchar(45) NOT NULL,
+  PRIMARY KEY (`mess_incharge_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mess`
+--
+
+LOCK TABLES `mess` WRITE;
+/*!40000 ALTER TABLE `mess` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mess` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `room_id` int NOT NULL,
+  `capacity` int NOT NULL,
+  `hostel_id` int NOT NULL,
+  `name_of_student` varchar(40) NOT NULL,
+  `room_status` varchar(10) NOT NULL,
+  PRIMARY KEY (`room_id`),
+  KEY `hostel_id` (`hostel_id`),
+  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`hostel_id`) REFERENCES `boys_hostel` (`hostel_id`),
+  CONSTRAINT `room_chk_1` CHECK (((`capacity` >= 0) and (`capacity` <= 3)))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff` (
+  `emp_name` varchar(40) NOT NULL,
+  `emp_id` int NOT NULL,
+  `emp_address` varchar(50) NOT NULL,
+  `emp_salary` int NOT NULL,
+  `emp_cellno` varchar(20) NOT NULL,
+  `hostel_id` int NOT NULL,
+  `emp_designation` varchar(40) NOT NULL,
+  `mess_incharge_Id` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`),
+  KEY `hostel_id` (`hostel_id`),
+  KEY `mess_incharge_Id` (`mess_incharge_Id`),
+  CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`hostel_id`) REFERENCES `boys_hostel` (`hostel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `staff_ibfk_2` FOREIGN KEY (`mess_incharge_Id`) REFERENCES `mess` (`mess_incharge_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `student_id` int NOT NULL,
+  `student_name` varchar(40) NOT NULL,
+  `student_father_name` varchar(40) DEFAULT NULL,
+  `department` varchar(30) NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `cell_no` varchar(20) NOT NULL,
+  `student_age` int NOT NULL,
+  `student_DOB` date NOT NULL,
+  `hostel_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  PRIMARY KEY (`student_id`),
+  KEY `hostel_id` (`hostel_id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `student_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-21 11:04:44
